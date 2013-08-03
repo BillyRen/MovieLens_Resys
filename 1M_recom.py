96d81befeefa3110d0cce9e067b76e910bdeda85
Billy Ren <renchengbin.billy@gmail.com>
2013-08-03 21:59:11 +0800

    UPDATE  recommendation function

--- 1M_recom.py
+++ 1M_recom.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+import math
+import random
+
 def splitData(data, M, k, seed):
     """Split data into test and train set randomly
     """
@@ -49,7 +53,8 @@ def coverage(train, test, N):
     allItems = set()
     
     for user in train.keys():
-        allItems.add(item)
+        for item in train[user].keys():
+            allItems.add(item)
     rank = getRecommendation(user, N)
     for item, puItem in rank:
         recommendItems.add(item)
@@ -75,3 +80,98 @@ def popularity(train, test, N):
             n += 1
     ret /= n * 1.0
     return ret
+    
+def userSimilarity(train):
+    """Calculate user similarity
+    """
+    W = {}
+    for u in train.keys():
+        for v in train.keys():
+            if u == v:
+                continue 
+            W[u][v] = len(train[u] & train[v])
+            W[u][v] /= math.sqrt(len(train[u]) * len(train[v]) * 1.0)
+    return W
+    
+def userSimilarityInverse(train):
+    """Calculate user similarity using inverse table
+    """
+    itemUsers = {}
+    for u, item in train.items():
+        for i in item.keys():
+            if i not in itemUsers:
+                itemUsers[i] = set()
+            itemUsers[i].add(u)
+            
+    C = {}
+    N = {}
+    for i, users in itemUsers.items():
+        for u in users:
+            N[u] += 1
+            for v in users:
+                if u == v:
+                    continue 
+                C[u][v] += 1
+                
+    W = {}
+    for u, relatedUsers in C.items():
+        for v, cUV in relatedUsers.items():
+            W[u][v] = cUV / math.sqrt(N[u] * N[v])
+    return W
+    
+def userSimilarityIIF(train):
+    itemUsers = {}
+    for u, item in train.items():
+        for i in item.keys():
+            if i not in itemUsers:
+                itemUsers[i] = set()
+            itemUsers[i].add(u)
+            
+    C = {}
+    N = {}
+    for i, users in itemUsers.items():
+        for u in users:
+            N[u] += 1
+            for v in users:
+                if u == v:
+                    continue 
+                C[u][v] += 1 / math.log(1 + len(users))
+    
+    W = {}
+    for u, relatedUsers in C.items():
+        for v, cUV in relatedUsers.items():
+            W[u][v] = cUV / math.sqrt(N[u] * N[v])
+    return W              
+        
+    
+def recommendationUser(user, train, W, K):
+    """userCF
+    """
+    rank = {}
+    interactedItems = train[user]
+    for v, wUV in sorted(W[user].items, key = itemgetter(1), reverse = True)[0:K]:
+        for i, rUV in train[v].items:
+            if i in interactedItems:
+                continue 
+            rank[i] += wUV * rUV
+    return rank
+    
+def itemSimilarity(train):
+    C = {}
+    N = {}
+    for user, item in train.items():
+        for i in user:
+            N[i] += 1
+            for j in user:
+                if i == j:
+                    continue 
+                C[i][j] += 1
+                
+    W = {}
+    for i, relatedItems in C.items():
+        for j, cIJ in relatedItems.items():
+            W[i][j] = cIJ / math.sqrt(N[i] * N[j])
+    return W
+    
+"""下一步要完成的：itemCF的推荐函数
+"""
